14f6d6a6fd9ca3137b227e9c59eb5dde964d3872
aleksi svaridze <aleksisvaridze@outlook.com>
2023-07-02 17:46:55 +0400

    Import Data array

---
+++ src/data/Data.js
@@ -0,0 +1,9 @@
+const accordionContent = [
+    {id: 1, title: 'How many team members can I invite?', desc: 'No more than 2GB. All files in your account must fit your allotted storage space.'},
+    {id: 2, title: 'What is the maximum file upload size?', desc: 'No more than 2GB. All files in your account must fit your allotted storage space.'},
+    {id: 3, title: 'How do I reset my password?', desc: 'No more than 2GB. All files in your account must fit your allotted storage space.'},
+    {id: 4, title: 'Can I cancel my subscription?', desc: 'No more than 2GB. All files in your account must fit your allotted storage space.'},
+    {id: 5, title: 'Do you provide additional support?', desc: 'No more than 2GB. All files in your account must fit your allotted storage space.'},
+];
+
+export default accordionContent;
