c6b914dbb6abfa1c7cac13bfe95d14262f13c230
aleksi svaridze <aleksisvaridze@outlook.com>
2023-07-01 22:58:00 -0700

    Add: background image to card

--- src/components/AccordionMain.js
+++ src/components/AccordionMain.js
@@ -1,29 +1,12 @@
 import styles from './Accordion.module.css';
-import { useState } from 'react';
+import boxImg from '../images/box.svg';
 
 
 export const Accordion = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    const [isVisible1, setIsVisible1] = useState(false);
-    const [isVisible2, setIsVisible2] = useState(false);
 
     return (
         <div className={styles.container}>
-
-            <div onClick={() => setIsVisible(!isVisible)} className={styles.card}>
-                <h3 className={styles.title}>accordion title</h3>
-                { isVisible ? <p className={styles.desc}>Lorem ipsum she schema...</p> : null }
-            </div>
-
-            <div onClick={() => setIsVisible1(!isVisible1)} className={styles.card}>
-                <h3 className={styles.title}>accordion title</h3>
-                { isVisible1 ? <p className={styles.desc}>Lorem ipsum she schema...</p> : null }
-            </div>
-
-            <div onClick={() => setIsVisible2(!isVisible2)} className={styles.card}>
-                <h3 className={styles.title}>accordion title</h3>
-                { isVisible2 ? <p className={styles.desc}>Lorem ipsum she schema...</p> : null }
-            </div>
+            <img src={boxImg} className={styles.box} alt='' />
 
         </div>
     )
