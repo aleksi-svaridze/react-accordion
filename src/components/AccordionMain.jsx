e68839e6f2cdccabdb9f87d51dec39c620f1275b
aleksi svaridze <aleksisvaridze@outlook.com>
2023-07-27 12:33:59 +0400

    Version for Testing

--- src/components/AccordionMain.jsx
+++ src/components/AccordionMain.jsx
@@ -1,11 +1,13 @@
-import styles from './Accordion.module.css';
 import boxImg from '../images/box.svg';
+import MobileBg from '../images/mobile_bg.svg';
 import { AccordionItem } from './accordion-item/AccordionItem';
 import { useState } from 'react';
+import { useWindowSize } from 'usehooks-ts';
 import accordionContent from '../data/Data';
 
 export const Accordion = () => {
     const [activeNumber, setActiveNumber] = useState(null);
+    const {width} = useWindowSize();
 
     const showDesc = (params) => {
         if(activeNumber === params) {
@@ -16,10 +18,12 @@ export const Accordion = () => {
     }
 
     return (
-        <div className={styles.container}>
-            <img src={boxImg} className={styles.box} alt='' />
-            <div className={styles.text_content}>
-                <h1 className={styles.text_content__title}>faq</h1>
+        <div className='card'>
+            {
+                width >= 1100 ? <img src={boxImg} className='card__box_img' alt='' /> : <img src={MobileBg} alt="" className='card__mobile_bg' />
+            }
+            <div className='card__text_content'>
+                <h1 className='card__text_content__title'>faq</h1>
 
                 {accordionContent.map(item => (
                      <AccordionItem 
--- src/components/accordion-item/AccordionItem.jsx
+++ src/components/accordion-item/AccordionItem.jsx
@@ -1,19 +1,18 @@
-import styles from './accordionItem.module.css';
 import ArrowDown from '../../images/arrow-down.svg';
 import ArrowUp from '../../images/arrow-top.svg';
 
 export const AccordionItem = ({id, title, desc, activeNumber, showDesc }) => {
 
     return (
-        <div className={styles.accordion__item} key={id}>
-            <h3 className={activeNumber !== id ? styles.accordion__title : styles.accordion__title_active +' '+ styles.active} onClick={() => showDesc(id)}>
+        <div className='faq' key={id}>
+            <h3 className={activeNumber !== id ? 'faq__title' : 'faq__title_active active'} onClick={() => showDesc(id)}>
                 {title} 
                 { activeNumber !== id
-                ? <img src={ArrowDown} className={styles.arrow_down} /> 
-                : <img src={ArrowUp} className={styles.arrow_down} />}
+                ? <img src={ArrowDown} alt='' className='arrow_down' /> 
+                : <img src={ArrowUp} alt='' className='arrow_down' />}
             </h3>
 
-            { activeNumber === id ?  <p className={styles.accordion__desc}>{desc}</p> : null}
+            { activeNumber === id ?  <p className='faq__desc'>{desc}</p> : null}
         </div>
     )
 }
